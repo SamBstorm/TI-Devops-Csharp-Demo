b9629b92cdd41c5ce50371eb4ca1c0050bed9b19
Samuel Legrain <samuel.legrain@bstorm.be>
2026-03-04 15:08:09 +0100

    Ajoutez des fichiers projet.

---
+++ Devops.Core/Aquarium.cs
@@ -0,0 +1,30 @@
+﻿namespace Devops.Core
+{
+    public class Aquarium
+    {
+        private DateTime _createdAt;
+        public DateTime CreatedAt
+        {
+            get
+            {
+                return _createdAt;
+            }
+            private set
+            {
+                _createdAt = value;
+            }
+        }
+
+        public int Duration
+        {
+            get {
+                return (int)(DateTime.Now - _createdAt).TotalSeconds;
+            }
+        }
+
+        public Aquarium()
+        {
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ Devops.Core/Fish.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devops.Core
+{
+    public enum FishSpecies { Tuna, ClowFish, Sole }
+    public class Fish
+    {
+        public static List<FishSpecies> CarnivoreSpecies = new List<FishSpecies>() {
+            FishSpecies.Tuna,
+            FishSpecies.ClowFish
+        };
+
+        private FishSpecies _specy;
+        public FishSpecies Specy
+        {
+            get { return _specy; }
+            private set { _specy = value; }
+        }
+
+        public bool IsCarnivore
+        {
+            get
+            {
+                return Fish.CarnivoreSpecies.Contains(_specy);
+            }
+        }
+
+        public Fish(FishSpecies specy)
+        {
+            Specy = specy;
+        }
+    }
+}
